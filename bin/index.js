221c23703b8242dddfe8c345a09b0022689790a6
liuzhiyang <13583106771@163.com>
2021-03-18 09:17:32 +0800

    fix: Complete support format files

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 const processor = (image) => {
-    if(/\.(png|jpg|webp)$/.test(image)){
+    if (/\.(png|jpg|webp)$/.test(image)) {
         return Promise.resolve();
     }
     const imageInCatalogArr = image.match(/(.*\/)(?:.*\.(?:png|jpg|webp))/);
-    const imageInCatalog =  imageInCatalogArr && imageInCatalogArr[1] ?  imageInCatalogArr[1]: '';
-    if(!imageInCatalog){
+    const imageInCatalog = imageInCatalogArr && imageInCatalogArr[1] ? imageInCatalogArr[1] : '';
+    if (!imageInCatalog) {
         return Promise.reject(`${image}： not know parent directory`);
     }
     return new Promise((resolve) => {
-        shell.exec(`squoosh-cli --mozjpeg {quality:75} -d ${imageInCatalog} ${image}`,{shell:true},(code, stdout, stderr) => {
+        let shell = '';
+        if (/\.jpg$/.test(image)) {
+            shell = `squoosh-cli --mozjpeg {quality:75} -d ./img ${image}`
+        }
+        if (/\.png$/.test(image)) {
+            shell = `squoosh-cli --oxipng auto --quant {enabled:true} -d ./img ${image}`;
+        }
+        if (/\.webp$/.test(image)) {
+            shell.exec(`squoosh-cli --webp {quality:60} -d ./img ${image}`);
+        }
+        if (shell) {
+            return Promise.resolve();
+        }
+        shell.exec(shell, { shell: true }, (code, stdout, stderr) => {
             console.log('Exit code:', code);
             console.log('Program output:', stdout);
             console.log('Program stderr:', stderr);
@@ -19,7 +32,7 @@ const processor = (image) => {
     })
 }
 
-Promise.all(process.argv.slice(2).map(processor)).catch(function(e) {
+Promise.all(process.argv.slice(2).map(processor)).catch(function (e) {
     console.error(e);
     process.exit(1);
 });
